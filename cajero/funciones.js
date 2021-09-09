a496460163817bfe3b1f037952f743aafa9d4e7f
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2021-09-09 17:06:51 -0500

    cajero base

---
+++ cajero/funciones.js
@@ -0,0 +1,19 @@
+// Definir el usuario y la contraseña o el pin
+
+let pinHiromi = 12345;
+
+function ingresar() {
+    let pin = document.getElementById("pin").value;
+
+    // Revisar si el PIN es correcto, usuario y contraseña
+    // Si es correcto, te deja ir a la segunda sección
+    // Si es incorrecto, te muestra un mensaje
+}
+
+function retirar () {
+
+}
+
+function ahorrar () {
+
+}
