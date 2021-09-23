c4322ca2c6329ed1e6032d9b59120d04c3e83612
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2021-09-23 16:04:07 -0500

    preguntas

---
+++ Intro-Async/preguntas1.js
@@ -0,0 +1,20 @@
+// PREGUNTAS PARTE 1
+
+// 1. ¿Qué tipo de lenguaje es javaScript?
+// 
+// 
+// 2. ¿Qué es algo síncrono?
+// 
+// 
+// 3. Escribe un ejemplo de la vida real que sea síncrono.
+// 
+// 
+// 4. ¿Qué es algo asíncrono?
+// 
+// 
+// 5. Escribe un ejemplo de la vida real que sea asíncrono.
+// 
+// 
+// 6. ¿JavaScript es un lenguaje síncrono o asíncrono? ¿Por qué?
+// 
+// 
---
+++ Intro-Async/preguntas2.js
@@ -0,0 +1,37 @@
+// PREGUNTAS PARTE 2
+// 1. ¿Qué es una función flecha en javaScript?
+// 
+// 
+// 2. ¿Cuál es la diferencia entre una función flecha y una función convencional?
+// 
+// 
+// 3. Transforma las siguientes funciones a funciones flecha.
+// 
+// 
+// 4. Transforma las siguientes funciones convencionales a funciones flecha
+
+// Función convencional
+
+function sumar (num1, num2) {
+    let resultado = num1 + num2;
+    return resultado;
+}
+
+// Función flecha
+
+
+
+// ----------------------------------------------------------
+
+//Función convencional
+
+function transformar (palabra) {
+    let palabraTransformada = palabra.toUpperCase();
+    return palabraTransformada;
+}
+
+// Función flecha
+
+
+
+// ----------------------------------------------------------
