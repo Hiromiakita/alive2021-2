be74ad014b58bf36bac2e4ecabf9d93a1ddef6dc
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2021-09-30 15:52:59 -0500

    data

---
+++ manejo_datos/index.js
@@ -0,0 +1,35 @@
+let info = {
+    statusCode: 200,
+    data: [
+        {
+            nombre: 'Sofía',
+            ciudad: 'Oaxaca'
+        },
+        {
+            nombre: 'Laura',
+            ciudad: 'Tabasco'
+        },
+        {
+            nombre: 'América',
+            ciudad: 'Guadalajara'
+        },
+        {
+            nombre: 'Ana',
+            ciudad: 'Monterrey'
+        },
+        {
+            nombre: 'Diana',
+            ciudad: 'Oaxaca'
+        }
+    ]
+};
+
+
+
+// ¿Cómo muestro el siguiente mensaje? -> Su status code es: (inserte el número del status code)
+
+// ¿Cómo mostramos sólo lo que hay en data?
+
+// ¿Cómo creo un arreglo con sólo los nombres?
+
+// ¿Cómo creo un arreglo con los usuarios que viven en Oaxaca?
