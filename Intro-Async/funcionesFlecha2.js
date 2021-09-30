c1bd9832c246f27ac7bc6072d15d2ce6d1eef807
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2021-09-30 18:01:19 -0500

    funciones flecha 2

---
+++ Intro-Async/funcionesFlecha2.js
@@ -0,0 +1,23 @@
+// Función convencional
+function mostrarPrimeraPosicion (array) {
+    let pos1 = array[0];
+    return pos1;
+}
+
+// Función flecha
+
+
+
+// -----------------------------------------------------------
+
+// Función convencional
+function definirValorMayor (num1, num2) {
+    let valorMayor = num1;
+    if (num2 > num1) {
+        valorMayor = num2;
+    }
+
+    return valorMayor;
+}
+
+// Función flecha
