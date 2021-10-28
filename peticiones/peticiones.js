ec4d296594cbec694f086b814a5f7fea5560aa0e
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2021-10-28 18:03:29 -0500

    peticiones

--- peticiones/peticiones.js
+++ peticiones/peticiones.js
@@ -11,7 +11,14 @@ let traerUnPersonajeSW = () => {
 
     fetch('https://swapi.dev/api/people/1/')
         .then(info => info.json())
-        .then(infoTransformada => console.log(infoTransformada));
+        .then(infoTransformada => {
+            console.log(infoTransformada);
+            console.log(infoTransformada.name);
+            console.log(infoTransformada.birth_year);
+
+            document.getElementById("nombre").innerText = infoTransformada.name;
+            document.getElementById("anio").innerText = infoTransformada.birth_year;
+        });
 }
 
 let traerPaises = () => {
@@ -21,3 +28,29 @@ let traerPaises = () => {
         .then(info => info.json())
         .then(infoJson => console.log(infoJson));
 }
+
+let traerPokemones = () => {
+    console.log('trayendo pokemones');
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=5")
+        .then(info => info.json())
+        .then(infoTransformada => {
+            console.log(infoTransformada);
+            // console.log(infoTransformada.results[0].name)
+            // console.log(infoTransformada.results[1].name)
+            // console.log(infoTransformada.results[2].name)
+            // console.log(infoTransformada.results[3].name)
+            // console.log(infoTransformada.results[4].name)
+
+            let seccion = document.getElementById("pokemones");
+            for(let i = 0; i < infoTransformada.results.length; i++) {
+                console.log(infoTransformada.results[i].name);
+
+                //crear una etiqueta p por cada pokemon
+                let parrafo = document.createElement("p");
+                seccion.append(parrafo);
+                parrafo.innerText = infoTransformada.results[i].name;
+            }
+        })
+}
+
